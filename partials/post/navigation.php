63c8eb22920ceee42d7f2bc86410a94eaa261a5f
AJ Clarke <wpexplorer@gmail.com>
2019-03-16 22:50:00 -0700

    Fixed issue with WordPress adding nav-links to single post pagination which is also added in the the_posts_pagination (numbered pagination) causing CSS conflicts

--- partials/post/navigation.php
+++ partials/post/navigation.php
@@ -16,11 +16,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Get post navigation
 $args     = apply_filters( 'pwd_post_navigation_args', array(
-	'prev_text'	=> '<div class="pwd-label">'. esc_html__( 'Next Article', 'wpex-powered' ) .'<span class="fa fa-caret-right"  aria-hidden="true"></span></div><div class="pwd-link">%title</div>',
+	'prev_text'	=> '<div class="pwd-label">'. esc_html__( 'Next Article', 'wpex-powered' ) . '<span class="fa fa-caret-right"  aria-hidden="true"></span></div><div class="pwd-link">%title</div>',
 	'next_text'	=> '<div class="pwd-label"><span class="fa fa-caret-left" aria-hidden="true"></span>'. esc_html__( 'Previous Article', 'wpex-powered' ) .'</div><div class="pwd-link">%title</div>',
 ) );
 $post_nav = get_the_post_navigation( $args );
 $post_nav = str_replace( 'role="navigation"', '', $post_nav );
+$post_nav = str_replace( 'nav-links', 'pwd-nav-links', $post_nav );
 
 // Display post navigation
 if ( ! is_attachment() && $post_nav ) : ?>
